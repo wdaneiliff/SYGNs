316cbd54d3a238cd4782f812f150813674e10089
Dane Iliff <wdaneiliff@gmail.com>
2015-08-19 20:42:35 -0700

    sign object filled half way with form

--- models/sygn.js
+++ models/sygn.js
@@ -1,27 +1,24 @@
 //BRING IN ALL REQUIRE PACKAGES
 var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
-var bcrypt      = require('bcrypt-nodejs');
+
 
 var SygnSchema = new Schema({
-    /*
-    firstName : String,
-    lastName : String,
-    email : { type: String, required: true, index: {unique:true} },
-    password : { type: String, required: true, select: false }
-    */
+
+    point1 : Number,
+    point2 : Number,
+    type: String,
+    monday:[],
+    tuesday:[],
+    wednesday:[[6,8],[4,6]],
+    thursday:[],
+    friday:[],
+    saturday:[],
+    sunday:[],
+    user_email: String
 });
 
-SygnSchema.pre('save', function(next){
-  var sygn = this;
 
-});
-
-//METHOD TO COMPARE PW WITH CURRENT DB password
-SygnSchema.methods.comparePassword = function(password){
-    var user = this;
-    return bcrypt.compareSync(password, user.password);
-};
 
 //EXPORT THE MODEL
 module.exports = mongoose.model("Sygn", SygnSchema);
